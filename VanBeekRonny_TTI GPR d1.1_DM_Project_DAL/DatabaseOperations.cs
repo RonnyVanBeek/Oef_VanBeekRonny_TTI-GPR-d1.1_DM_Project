c8901f4c91344e24e24aba33fbbcc1d0bbe9811b
Ronny Van Beek <r0849413@student.thomasmore.be>
2021-04-18 18:54:02 +0200

    Add project files.

---
+++ VanBeekRonny_TTI GPR d1.1_DM_Project_DAL/DatabaseOperations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VanBeekRonny_TTI_GPR_d1._1_DM_Project_DAL
+{
+    public class DatabaseOperations
+    {
+    }
+}
---
+++ VanBeekRonny_TTI GPR d1.1_DM_Project_WPF/Films.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VanBeekRonny_TTI_GPR_d1._1_DM_Project_WPF
+{
+    /// <summary>
+    /// Interaction logic for Films.xaml
+    /// </summary>
+    public partial class Films : Window
+    {
+        public Films()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //dataFilms.ItemsSource = DatabaseOperations.OphalenFilms();
+        }
+
+        private void btnSluiten_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
