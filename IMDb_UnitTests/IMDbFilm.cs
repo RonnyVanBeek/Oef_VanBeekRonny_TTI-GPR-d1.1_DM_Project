90c1579c0441b601d074676d9f124ce85f3c83f9
Ronny Van Beek <r0849413@student.thomasmore.be>
2021-05-13 14:19:14 +0200

    - Toevoegen UnitTests aan het project

---
+++ IMDb_UnitTests/IMDbFilm.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using VanBeekRonny_TTI_GPR_d1._1_DM_Project_Models;
+
+namespace IMDb_UnitTests
+{
+    [TestClass]
+    public class IMDbFilm
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ IMDb_UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IMDb_UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IMDb_UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7f3f734a-056a-4daa-8beb-12512da4f703")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
